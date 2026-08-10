76834f44516a687feb4e14dfafa61bc0fc4a9d87
Gabriel <gabriel.rezende131101@gmail.com>
2026-08-10 16:42:24 -0300

    Estrutura inicial do spring

---
+++ src/main/java/br/com/video2frames/video2frames_video_service/Video2framesVideoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.video2frames.video2frames_video_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Video2framesVideoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Video2framesVideoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/video2frames/video2frames_video_service/Video2framesVideoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.video2frames.video2frames_video_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Video2framesVideoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
